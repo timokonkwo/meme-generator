66c3e3c3bfbd1e6829c26bf5ec8b70771d5176c0
Tim <chibuiketimokonkwo@gmail.com>
2022-09-16 17:03:33 +0100

    implemented useState in setting the img

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,27 @@
 import memesData from "../assets/memesData";
+import { useState } from "react";
+
 
 export default function Meme() {
-	const memes = memesData.data.memes;
-	const imgUrl = memes[Math.floor(Math.random() * memes.length)].url;
-	console.log(imgUrl)
+	function getImg(){
+		const memes = memesData.data.memes;
+		const imgUrl = memes[Math.floor(Math.random() * memes.length)].url;
+
+		setImg(prev => imgUrl)
+	}
+
+	const [img, setImg] = useState(null);
+
 	return (
 		<main className="container">
 			<div className="input-container">
 				<input type="text" placeholder="top text" />
 				<input type="text" placeholder="bottom text" />
 			</div>
-			<button>Get a new meme image 👀</button>
+			<button onClick={getImg}>Get a new meme image 👀</button>
 
 			<div className="container">
-				<img src={imgUrl} />
+				<img src={img} />
 			</div>
 		</main>
 	);
