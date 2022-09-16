164c6e5d78b2db0b662844b37c061241586de1a3
Tim <chibuiketimokonkwo@gmail.com>
2022-09-16 13:05:28 +0100

    updates

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,19 +1,11 @@
 export default function Meme() {
 	return (
 		<main className="container">
-			<form>
-				<div className="input-container">
-					<input 
-                        type="text"
-                        placeholder="top text"
-                     />
-					<input 
-                        type="text" 
-                        placeholder="bottom text"
-                    />
-				</div>
-				<button>Get a new meme image</button>
-			</form>
+			<div className="input-container">
+				<input type="text" placeholder="top text" />
+				<input type="text" placeholder="bottom text" />
+			</div>
+			<button>Get a new meme image 👀</button>
 		</main>
 	);
 }
