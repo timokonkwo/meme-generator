4e4cfaf53068d01118b1c4abab054453b57e9fce
Tim <chibuiketimokonkwo@gmail.com>
2022-09-19 11:50:02 +0100

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 	return (
 		<div className="navbar">
 			<div className="logo">
-				<img src={trollface} alt="troll face emoji"/>
+				<img src={trollface} alt="troll face emoji icon"/>
                 Meme Generator
 			</div>
 
