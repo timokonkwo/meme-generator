8eba65da15fa08ae10c2ae4e63fea07dc4befe10
Tim <chibuiketimokonkwo@gmail.com>
2022-09-19 11:35:40 +0100

    auto commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ export default function Navbar() {
 	return (
 		<div className="navbar">
 			<div className="logo">
-				<img src={trollface} alt="" />
+				<img src={trollface} />
                 Meme Generator
 			</div>
 
             <p>
-				<a href="https://github.com/timokonkwo/meme-generator">
+				<a href="https://github.com/timokonkwo/meme-generator" target="_blank">
 					
 				<img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="" className="github-icon"/>
 					View on github
